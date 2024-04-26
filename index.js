d0be3a7fe662460aa0bc2689eb6b95c69fe52c8e
impactgalaxy <traderglow@gmail.com>
2024-04-26 08:45:43 +0600

    create server setup

---
+++ index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const a = process.env.PASS
